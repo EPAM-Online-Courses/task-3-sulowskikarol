ba94b0a001aaa51b4d25bb1bda1611d44a2c2474
sulowskikarol <karolsu128@gmail.com>
2023-05-21 19:37:02 +0200

    Zadanie 1

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -2,6 +2,18 @@ package efs.task.oop;
 
 public class Main {
     public static void main(String[] args) {
+        Villager kashya = new Villager("Kashya", 30);
+        Villager akara = new Villager("Akara", 40);
+        Villager gheed = new Villager("Gheed", 50);
+        Villager deckard = new Villager("Deckard Cain", 85);
+        Villager warriv = new Villager("Warriv", 35);
+        Villager flawia = new Villager("Flawia", 25);
 
+        kashya.sayHello();
+        akara.sayHello();
+        gheed.sayHello();
+        deckard.sayHello();
+        warriv.sayHello();
+        flawia.sayHello();
     }
 }
